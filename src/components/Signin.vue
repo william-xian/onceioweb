5b6056e4fa3b5d0a70a7b3c0ea1ff86741f4d681
Liar <1520070240@qq.com>
2018-07-01 20:56:59 +0800

    添加登录接口

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,7 +1,14 @@
 <template>
 	<div>
-        <input class="down" type="text" name="cmd"/>
-        <button @click="input">确认</button>
+
+    <div class="down input-group">
+      <select name="opt">
+        <option value="1">添加</option>
+        <option value="2">查询</option>
+      </select>
+      <input id="input" type="text" class="form-control" placeholder="A > B; B < A; A >< B; A <> B; A = B; A,B > C;">
+      <a class='btn btn-info input-group-addon' @click="input">Submit</a>
+    </div>
 	</div>
 </template>
 
@@ -14,10 +21,22 @@ export default {
     }
   },
   mounted() {
+    console.log(this.getQueryString("code"));
 
   },
   methods : {
       input :function() {
+       var v = document.getElementById("input");
+       console.log(v.value);
+      },
+      getQueryString :function(name) {
+        var uri = window.location.href;
+        var i = uri.lastIndexOf('?');
+        uri = uri.substr(i+1);
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = uri.match(reg);
+        if (r != null) return unescape(r[2]); 
+        return null; 
       }
   }
 }
